17f43ae3fcb33ad5641c34e68a5236240efde583
JackTitor <honzis0@gmail.com>
2022-12-26 04:10:09 +0100

    Set proper program name to Annie

--- build.rs
+++ build.rs
@@ -19,6 +19,8 @@ fn main() {
         println!("cargo:rerun-if-changed=resource/annie-main.ico");
         WindowsResource::new()
             .set_icon("resource/annie-main.ico")
+            .set("ProductName", "Annie")
+            .set("FileDescription", "Annie")
             .compile()
             .unwrap();
     }
