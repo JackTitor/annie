3e4b4189191620ba160686f7810a4c56c9e4bef8
JackTitor <honzis0@gmail.com>
2023-01-27 20:00:23 +0100

    Change data folder to %LocalAppData%\annie\

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,13 @@ mod tray_application;
 mod window;
 mod window_listener;
 
-use std::{env, fs::File, panic, path::PathBuf, sync::mpsc};
+use std::{
+    env,
+    fs::{self, File},
+    panic,
+    path::{Path, PathBuf},
+    sync::mpsc,
+};
 
 use log::{info, LevelFilter};
 use msgbox::IconType;
@@ -20,15 +26,17 @@ fn main() {
     let Some(_instance_lock) = get_instance_lock() else { return };
 
     set_panic_hook();
-    setup_logger();
+
+    let data_dir = get_or_create_data_dir();
+    let config_path = data_dir.join("annie.toml");
+
+    setup_logger(&data_dir);
 
     info!("Annie start");
 
     let (core_sender, core_receiver) = mpsc::channel();
     let (_tray_thread, tray_sender) = tray_application::create_tray_thread(core_sender.clone());
     let listener_thread = window_listener::WindowListenerHandle::spawn(core_sender);
-
-    let config_path = get_data_dir().join("annie.toml");
 
     AnnieCore::run_with_config(config_path, core_receiver, tray_sender, listener_thread);
 
@@ -48,17 +56,20 @@ fn get_instance_lock() -> Option<SingleInstance> {
     }
 }
 
-fn get_data_dir() -> PathBuf {
+fn get_or_create_data_dir() -> PathBuf {
     if cfg!(debug_assertions) {
         let mut path = env::current_exe().expect("cannot locate current exe");
         path.pop();
         path
     } else {
-        dirs::document_dir().expect("cannot locate documents dir")
+        let mut path = dirs::data_local_dir().expect("cannot locate LocalAppData");
+        path.push("annie");
+        fs::create_dir_all(&path).expect("cannot create data dir");
+        path
     }
 }
 
-fn setup_logger() {
+fn setup_logger(data_dir: impl AsRef<Path>) {
     if cfg!(debug_assertions) {
         TermLogger::init(
             LevelFilter::Debug,
@@ -68,7 +79,7 @@ fn setup_logger() {
         )
         .expect("cannot initialize debug mode logger");
     } else {
-        let log_path = get_data_dir().join("annie.log");
+        let log_path = data_dir.as_ref().join("annie_log.log");
         let log_file = File::create(log_path).expect("cannot write to log file");
         WriteLogger::init(LevelFilter::Info, Config::default(), log_file)
             .expect("cannot initialize logger");
